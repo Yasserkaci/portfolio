7bd74d7487818c5f9f8c0af395b3d5fec97bb882
unknown <kaciyasserabdeldjalil@gmail.com>
2024-02-21 23:23:37 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Header from './componentes/1-header/Header'
+import Hero from './componentes/2-hero/Hero'
+import Main from './componentes/3-main/Main'
+import Contact from './componentes/4-contact/Contact'
+import Footer from './componentes/5-footer/Footer'
+
+
+
+
+function App() {
+
+
+  return (
+     <div className='contaner'>     
+        <Header />
+        <div className="divider"/>
+        <Hero />
+        <div className="divider"/>
+        <Main />
+        <div className="divider"/>
+        <Contact />
+        <div className="divider"/>
+        <Footer/>
+     </div>
+  )
+}
+
+export default App
---
+++ src/componentes/1-header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <header className='flex'>
+      <nav>
+        <ul className="flex">
+          <li><a href="">About</a></li>
+          <li><a href="">Articles</a></li>
+          <li><a href="">Projects</a></li>
+          <li><a href="">Speaking</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+
+
+      <button>
+        light
+      </button>
+    </header>
+  )
+}
+
+export default Header

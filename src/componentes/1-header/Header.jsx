24e5658cb0e5384c7307d55f7d94274660e2aa7c
unknown <kaciyasserabdeldjalil@gmail.com>
2024-02-23 01:04:13 +0100

    contrnue on the navbar

--- src/componentes/1-header/Header.jsx
+++ src/componentes/1-header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useState } from "react";
 import './header.css'
 
 
 const Header = () => {
+  const [showmodel, setshowmodel] = useState(false);
   return (
     <header className='flex'>
+      <button className='menu icon-menu' onClick={() => { setshowmodel(true) }} ></button>
+
       <div></div>
       <nav>
         <ul className="flex">
@@ -17,9 +21,21 @@ const Header = () => {
       </nav>
 
 
-      <button>
-        light
-      </button>
+      <button ><span className='icon-moon-o'></span></button>
+
+      {showmodel &&
+        <div className="fixed">
+          <ul className="model">
+            <li><button onClick={() => { setshowmodel(false) }} className='icon-close'></button></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Articles</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Speaking</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>}
+
+
     </header>
   )
 }

fa56539892245ad1da02b86c768206d6bde858ad
unknown <kaciyasserabdeldjalil@gmail.com>
2024-03-03 21:29:27 +0100

    yaser

--- src/componentes/1-header/Header.jsx
+++ src/componentes/1-header/Header.jsx
@@ -20,24 +20,3 @@ const Header = () => {
         </ul>
       </nav>
 
-
-      <button className='mode flex'><span className='icon-moon-o'></span></button>
-
-      {showmodel &&
-        <div className="fixed">
-          <ul className="model">
-            <li><button onClick={() => { setshowmodel(false) }} className='icon-close'></button></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Articles</a></li>
-            <li><a href="">Projects</a></li>
-            <li><a href="">Speaking</a></li>
-            <li><a href="">Contact</a></li>
-          </ul>
-        </div>}
-
-
-    </header>
-  )
-}
-
-export default Header

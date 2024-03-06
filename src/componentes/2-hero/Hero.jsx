b5ba42d344f9c8350010e444677ca65cdb2ca5be
unknown <kaciyasserabdeldjalil@gmail.com>
2024-03-06 21:06:11 +0100

    hero section

--- src/componentes/2-hero/Hero.jsx
+++ src/componentes/2-hero/Hero.jsx
@@ -4,10 +4,17 @@ import './hero.css'
 const Hero = () => {
   return (
     <section className="parent flex">
-      <div className="left-section border">
+      <div className="left-section ">
         <div className="logo-section flex" >
-          <img src="./me.png" alt=""  className='logo'/>
+          <img src="./me.jpg" alt=""  className='logo'/>
           <div className='icon-verified'></div>
+        </div>
+        <h1 className="title">Unixes ! <br/> All freelancing services in one place</h1>
+        <p className="discreption">Welcome to Unixes! Your one-stop shop for freelance services. From writing to marketing, design, websites, and more, we've got you covered. Our team ensures quality results to bring your ideas to life. Let's work together and make it happen!</p>
+        <div className="icons flex">
+          <a href='https://www.linkedin.com/company/unixes'><div className="icon-linkedin-square"></div></a>
+          <a href='https://www.instagram.com/unixesagency/'><div className="icon-instagram"></div></a>
+          <a href=''><div className="icon-github"></div></a>
         </div>
       </div>
       <div className="right-section border">animation</div>

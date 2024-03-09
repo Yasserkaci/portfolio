382aed852582a13b74680a6af639e608d371a3dc
unknown <kaciyasserabdeldjalil@gmail.com>
2024-03-09 23:50:25 +0100

    adding light mode

--- src/componentes/1-header/Header.jsx
+++ src/componentes/1-header/Header.jsx
@@ -1,42 +1,106 @@
-import React from 'react'
-import { useState } from "react";
-import './header.css'
-
+import { useEffect, useState } from "react";
+import "./header.css";
 
 const Header = () => {
-  const [showmodel, setshowmodel] = useState(false);
-  return (
-    <header className='flex'>
-      <button className='menu icon-menu flex' onClick={() => { setshowmodel(true) }} ></button>
+  const [showModal, setshowModal] = useState(false);
+  const [theme, setTheme] = useState(
+    localStorage.getItem("currentMode") ?? "dark"
+  );
 
-      <div></div>
+  useEffect(() => {
+    if (theme === "light") {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    }
+  }, [theme]);
+
+  return (
+    <header className="  flex">
+      <button
+        onClick={() => {
+          setshowModal(true);
+        }}
+        className="menu icon-menu flex"
+      >
+        {" "}
+      </button>
+      <div />
+
       <nav>
         <ul className="flex">
-          <li><a href="">About</a></li>
-          <li><a href="">Members</a></li>
-          <li><a href="">Projects</a></li>
-          <li><a href="">Speaking</a></li>
-          <li><a href="">Contact</a></li>
+          <li>
+            <a href="">About</a>
+          </li>
+
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
         </ul>
       </nav>
 
-      <button className='mode flex'><span className='icon-moon-o'></span></button>
+      <button
+        onClick={() => {
+          // Send value to LS
+          localStorage.setItem(
+            "currentMode",
+            theme === "dark" ? "light" : "dark"
+          );
 
-      {showmodel &&
+          // get value from LS
+          setTheme(localStorage.getItem("currentMode"));
+        }}
+        className="mode flex"
+      >
+        {theme === "dark" ? (
+          <span className="icon-moon-o"> </span>
+        ) : (
+          <span className="icon-wb_sunny"> </span>
+        )}
+      </button>
+
+      {showModal && (
         <div className="fixed">
-          <ul className="model">
-            <li><button onClick={() => { setshowmodel(false) }} className='icon-close'></button></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Members</a></li>
-            <li><a href="">Projects</a></li>
-            <li><a href="">Speaking</a></li>
-            <li><a href="">Contact</a></li>
+          <ul className="modal ">
+            <li>
+              <button
+                className="icon-close"
+                onClick={() => {
+                  setshowModal(false);
+                }}
+              />
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Articles</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Speaking</a>
+            </li>
+            <li>
+              <a href="">Uses</a>
+            </li>
           </ul>
-        </div>}
-
-
+        </div>
+      )}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

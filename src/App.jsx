54a348aca171117cdde0bef6af7f8613ddcf024d
unknown <kaciyasserabdeldjalil@gmail.com>
2024-03-11 21:56:39 +0100

    destroying my computer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Card from './componentes/3-cards/Card'
 import Main from './componentes/4-main/Main'
 import Contact from './componentes/5-contact/Contact'
 import Footer from './componentes/6-footer/Footer'
+import { Helmet,  } from 'react-helmet-async';
+
 
 
 
@@ -11,21 +13,3 @@ import Footer from './componentes/6-footer/Footer'
 function App() {
 
 
-   return (
-      <div className='contaner'>     
-         <Header />
-         <Hero />
-         <div className="divider"/>
-         <Card/>
-         <div className="divider"/>
-         <Main />
-         <div className="divider"/>
-         <Contact />
-         <div className="divider"/>
-         <Footer/>
-      </div>
-   )
- }
- 
- export default App
- 
